d41e3e7f2358fc4fce2bcb36899db84265b1ada7
Lucas Caldeira <mrrealcaldeira@gmail.com>
2021-07-06 21:08:03 -0300

    api

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -16,6 +16,7 @@ class CarsRepository implements ICarsRepository {
     { lastPulledVersion })
     .getMany();     
     
+    
     return cars;
   }
 
--- src/modules/cars/useCases/sync/SyncPullCarsCase.ts
+++ src/modules/cars/useCases/sync/SyncPullCarsCase.ts
@@ -19,10 +19,13 @@ class SyncPullCarsCase {
   async execute(lastPulledVersion: number): Promise<ResponseData> {
     const updated = await this.usersRepository.listByUpdated(lastPulledVersion);
     const created = await this.usersRepository.listByCreated(lastPulledVersion);
+
+    const onlyNews = created.filter(car => car.created_at == car.updated_at);
+    const onlyUpdated = created.filter(car => car.created_at != car.updated_at);
     
     return {
-      created,
-      updated,
+      created: onlyNews,
+      updated: onlyUpdated,
       deleted: [],
     }
   }
